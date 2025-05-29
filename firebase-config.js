42d861f7ffe2e70d112b4d708cec4edcb28412ad
Feng Yuan <fengyuan3426@gmail.com>
2025-05-29 19:46:01 +1000

    Noteworthy Login Page
    Unfinished but it works! Firebase as our database

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC-j0NeTVLlnt2gyaWEmy7U8PvemvvtUE",
+  authDomain: "noteworthy-28a74.firebaseapp.com",
+  projectId: "noteworthy-28a74",
+  storageBucket: "noteworthy-28a74.firebasestorage.app",
+  messagingSenderId: "83941648213",
+  appId: "1:83941648213:web:ffbdb50225c9cbf88531dc"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+
